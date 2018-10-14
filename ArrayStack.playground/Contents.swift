5e806c41b7aafa8d307d614b2483f70375b3198c
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-14 22:53:18 +0900

    ArrayStack 2

--- ArrayStack.playground/Contents.swift
+++ ArrayStack.playground/Contents.swift
@@ -5,10 +5,17 @@ class ArrayStack<T> {
     func size() -> Int {
         return count
     }
+
+    func get(_ index: Int) -> T? {
+        guard 0 <= index,  index < count else { return nil}
+        return array[index]
+    }
 }
 
 // Test
 var hoge = ArrayStack<Int>()
 
 assert(hoge.size() == 0)
+assert(hoge.get(0) == nil)
+assert(hoge.get(-1) == nil)
 

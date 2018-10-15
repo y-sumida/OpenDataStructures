3ea027d0dd29521c5a31aeb45080b80931a6f116
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-15 22:31:30 +0900

    ArrayStack 3

--- ArrayStack.playground/Contents.swift
+++ ArrayStack.playground/Contents.swift
@@ -6,9 +6,19 @@ class ArrayStack<T> {
         return count
     }
 
-    func get(_ index: Int) -> T? {
+    func get(at index: Int) -> T? {
         guard 0 <= index,  index < count else { return nil}
         return array[index]
+    }
+
+    func add(at index: Int, element: T) {
+        // Todo resize()
+        guard 0 <= index,  index <= count else { return }
+        var newArray: [T] = array[0..<index].map { $0 }
+        newArray.append(element)
+        newArray.append(contentsOf: array[index..<count].map { $0 })
+        array = newArray
+        count += 1
     }
 }
 
@@ -16,6 +26,14 @@ class ArrayStack<T> {
 var hoge = ArrayStack<Int>()
 
 assert(hoge.size() == 0)
-assert(hoge.get(0) == nil)
-assert(hoge.get(-1) == nil)
+assert(hoge.get(at: 0) == nil)
+assert(hoge.get(at: -1) == nil)
+
+hoge.add(at: 0, element: 99)
+assert(hoge.size() == 1)
+assert(hoge.get(at: 0) == 99)
+hoge.add(at: 0, element: 1)
+assert(hoge.size() == 2)
+assert(hoge.get(at: 0) == 1)
+assert(hoge.get(at: 1) == 99)
 

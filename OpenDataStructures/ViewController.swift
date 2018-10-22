0666ef148477a2f7ef1b6bbe2fcba80b577004e3
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-22 21:57:04 +0900

    Playgroundからプロジェクトに変更

---
+++ OpenDataStructures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenDataStructures
+//
+//  Created by Yuki Sumida on 2018/10/22.
+//  Copyright © 2018年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ OpenDataStructuresTests/ArrayStackTest.swift
@@ -0,0 +1,47 @@
+import XCTest
+@testable import OpenDataStructures
+
+class ArrayStackTests: XCTestCase {
+    var arrayStack = ArrayStack<Int>()
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func testArrayInitialCount() {
+        XCTAssertEqual(arrayStack.size(), 0)
+    }
+
+    func testArrayAdd() {
+        arrayStack.add(at: 0, element: 99)
+        arrayStack.add(at: 1, element: 100)
+        XCTAssertEqual(arrayStack.size(), 2)
+        XCTAssertEqual(arrayStack.get(at: 0), 99)
+        XCTAssertEqual(arrayStack.get(at: 1), 100)
+    }
+
+    func testArraySet() {
+        arrayStack.add(at: 0, element: 99)
+        arrayStack.add(at: 1, element: 100)
+        let result = arrayStack.set(at: 1, element: 101)
+        XCTAssertEqual(result, 100)
+        XCTAssertEqual(arrayStack.get(at: 1), 101)
+    }
+
+    func testArrayRemove() {
+        arrayStack.add(at: 0, element: 99)
+        arrayStack.add(at: 1, element: 100)
+        let _ = arrayStack.remove(at: 1)
+        XCTAssertEqual(arrayStack.size(), 1)
+        XCTAssertEqual(arrayStack.get(at: 0), 99)
+    }
+
+    func testArrayRemoveAll() {
+        arrayStack.add(at: 0, element: 99)
+        arrayStack.add(at: 1, element: 100)
+        arrayStack.removeAll()
+        XCTAssertEqual(arrayStack.size(), 0)
+    }
+}

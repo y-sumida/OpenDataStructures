b8257a7118e9bbadfaeab4d904f35fe8f533423a
Yuki Sumida <y.sumida.0201@gmail.com>
2018-10-25 22:57:26 +0900

    ArrayDequeue 2

--- OpenDataStructures/ArrayDequeue.swift
+++ OpenDataStructures/ArrayDequeue.swift
@@ -1,9 +1,29 @@
-//
-//  ArrayDequeue.swift
-//  OpenDataStructures
-//
-//  Created by Yuki Sumida on 2018/10/25.
-//  Copyright © 2018年 Yuki Sumida. All rights reserved.
-//
+class ArrayDequeue<T> {
+    private var array: [T?] = [nil]
+    private var head = 0
+    private var count = 0
 
-import Foundation
+    private func resize() {
+        var newArray = [T?](repeating: nil, count: max(2 * count, 1))
+        for i in 0..<count {
+            newArray[i] = array[(head + i) % array.count]
+        }
+        array = newArray
+        head = 0
+    }
+
+    func size() -> Int {
+        return count
+    }
+
+    func get(at index: Int) -> T {
+        guard let t = array[(head + index) % array.count] else { fatalError("Index out of range.") }
+        return t
+    }
+
+    func set(at index: Int, element: T) -> T {
+        guard let t = array[(head + index) % array.count] else { fatalError("Index out of range.") }
+        array[(head + index) % array.count] = elemtnt
+        return t
+    }
+}

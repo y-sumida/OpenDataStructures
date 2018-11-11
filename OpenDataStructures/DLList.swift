1a2e939c067e4a8c4ff4c558a3c4e4236a1e005f
Yuki Sumida <y.sumida.0201@gmail.com>
2018-11-11 23:29:53 +0900

    DLList getNode,get,set実装

--- OpenDataStructures/DLList.swift
+++ OpenDataStructures/DLList.swift
@@ -11,16 +11,46 @@ class DLList<T> {
         }
     }
 
-    private var dummy: Node?
+    private var dummy: Node
     private var n: Int = 0
 
     init() {
         dummy = Node()
-        dummy?.next = dummy
-        dummy?.prev = dummy
+        dummy.next = dummy
+        dummy.prev = dummy
     }
 
     func size() -> Int {
         return n
+    }
+
+    func get(at index: Int) -> T {
+        guard let x = getNode(at: index).x else { fatalError("Index out of range.") }
+        return x
+    }
+
+    func set(at index: Int, value: T) -> T? {
+        let u = getNode(at: index)
+        let y = u.x
+        u.x = value
+        return y
+    }
+
+    private func getNode(at index: Int) -> Node {
+        var p: Node
+        if Double(index) < Double(n) / 2.0  {
+            p = dummy.next!
+            for _ in 0..<index {
+                p = p.next!
+            }
+        } else {
+            p = dummy
+            if index < n {
+                for _ in (index+1...n).reversed() {
+                    p = p.prev!
+                }
+            }
+        }
+        return p
     }
 }

7be6dd52c6cabb23e986a8a9d8b38d3c138b81cd
Yuki Sumida <y.sumida.0201@gmail.com>
2018-11-21 23:35:15 +0900

    SEList テストの準備

---
+++ OpenDataStructuresTests/SEListTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import OpenDataStructures
+
+class SEListTests: XCTestCase {
+    var list = SEList<Int>(block_size: 10)
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+}

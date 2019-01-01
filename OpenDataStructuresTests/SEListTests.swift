3d4e3502260e525487fb1b84213637bf9c38231d
Yuki Sumida <y.sumida.0201@gmail.com>
2019-01-01 12:12:33 +0900

    SEList setのテストを追加

--- OpenDataStructuresTests/SEListTests.swift
+++ OpenDataStructuresTests/SEListTests.swift
@@ -15,4 +15,17 @@ class SEListTests: XCTestCase {
         let x = list.get(i: 0)
         XCTAssertEqual(x, 10)
     }
+
+    func testSet() {
+        list.add(x: 10)
+        list.add(x: 20)
+        list.add(x: 30)
+        _ = list.set(i: 1, x: 99)
+        let x = list.get(i: 0)
+        XCTAssertEqual(x, 10)
+        let y = list.get(i: 1)
+        XCTAssertEqual(y, 99)
+        let z = list.get(i: 2)
+        XCTAssertEqual(z, 30)
+    }
 }

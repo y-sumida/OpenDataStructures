39b51a1acd423c1acef0d0ebc42d203dd6c3851f
Yuki Sumida <y.sumida.0201@gmail.com>
2019-01-24 22:12:34 +0900

    SkipListSSet Nodeの定義

--- OpenDataStructures/SkipListSSet.swift
+++ OpenDataStructures/SkipListSSet.swift
@@ -1,2 +1,13 @@
-class SkipListSSet<T> {
+class SkipListSSet<T: Comparable> {
+    class Node {
+        let x: T?
+        var height: Int = 0
+        var next: [Node] = []
+
+        init(value: T?, height: Int) {
+            x = value
+            self.height = height
+        }
+    }
+
 }

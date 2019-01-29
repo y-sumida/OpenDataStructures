dd4e3a80c39002237c4cb7d9c7e27d18d034e415
Yuki Sumida <y.sumida.0201@gmail.com>
2019-01-29 22:53:24 +0900

    SkipListSSet add の実装

--- OpenDataStructures/SkipListSSet.swift
+++ OpenDataStructures/SkipListSSet.swift
@@ -13,6 +13,7 @@ class SkipListSSet<T: Comparable> {
     private var sentinel: Node
     private var height: Int = 0
     private var n: Int = 0
+    private var stack: [Node] = []
 
     init() {
         sentinel = Node(value: nil, height: 32)
@@ -56,5 +57,41 @@ class SkipListSSet<T: Comparable> {
             return first?.x
         }
         return nil
+    }
+
+    func add(x: T) -> Bool {
+        var u = sentinel
+        var r = height
+        var comp = 0
+        while r >= 0 {
+            while u.next[r] != nil {
+                if let r = u.next[r], let _x = r.x {
+                    comp = compare(x: _x, y: x)
+                    if comp < 0 {
+                        u = r // リスト r の中で右に進む
+                    } else {
+                      break
+                    }
+                } else {
+                    break
+                }
+            }
+            if u.next[r] != nil && comp == 0 {
+               return false
+            }
+            r -= 1 // リスト r-1 下がる
+            stack[r] = u
+        }
+        let w = Node(value: x, height: pickHeight())
+        while height < w.height {
+            height += 1
+            stack[height] = sentinel // 高さが増える
+        }
+        for i in 0...w.height {
+            w.next[i] = stack[i].next[i]
+            stack[i].next[i] = w
+        }
+        n += 1
+        return true
     }
 }
